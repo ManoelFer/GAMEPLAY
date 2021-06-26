e086a58f70ef777f0f0da2d692b0086706df6f72
Manoel Fernandes <manoelfernandes15@gmail.com>
2021-06-26 11:32:58 -0300

    first commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { ViewContainer } from "./styles";
+
+export function SignIn() {
+  return (
+    <ViewContainer>
+      <Text>Hello world, NLW Together</Text>
+    </ViewContainer>
+  );
+}
